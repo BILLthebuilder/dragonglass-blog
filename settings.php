34c8bcff0143d31eb2d2e41a714d7ad2b4e01498
BILLthebuilder <billkariri@gmail.com>
2019-04-07 21:51:59 +0300

    add the settings page

---
+++ settings.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Site|settings</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/light-theme.css">
+</head>
+
+<body>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">Site settings</h1>
+            <!-- modify each li to point to its own modal  -->
+            <ul class="list-group d-flex justify-content-center">
+                <li class="list-group-item"><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#dragonGlass">
+                        Dragonglass themes
+                    </button>
+                </li>
+                <li class="list-group-item">Dragonblade themes</li>
+                <li class="list-group-item">Dragonslayers themes</li>
+                <li class="list-group-item">Dragonstone themes</li>
+                <li class="list-group-item">Dragonbone themes</li>
+            </ul>
+        </div>
+    </div>
+    </div>
+    </div>
+
+    </div>
+
+    <!-- start dragonglass themes modal -->
+    <div class="modal fade" id="dragonGlass" tabindex="-1" role="dialog" aria-labelledby="dragonGlassLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="dragonGlassLabel">Dragonglass themes</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <!-- theme list-->
+                    <p> link your themes here</p>
+                    <br>
+                    <a href="#"> light theme</a>
+                    <br>
+                    <a href="#"> dark theme</a>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end dragonglass themes modal -->
+
+    <!-- start dragongblade themes modal -->
+    <!-- end dragonblade themes modal -->
+
+    <!-- start dragonslayers themes modal -->
+    <!-- end dragonslayers themes modal -->
+
+    <!-- start dragonstone themes modal -->
+    <!-- end dragonstone themes modal -->
+
+    <!-- start dragonbones themes modal -->
+    <!-- end dragonbones themes modal -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+
+</html>
